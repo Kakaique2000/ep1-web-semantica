4ccc918bf6068249bac67f23c5c5228ff9ca5e64
Thiago Monica <63262830+ThiagoMonica@users.noreply.github.com>
2021-07-23 01:27:12 -0300

    commented: new queries functions

--- src/main/java/br/usp/haroldo/epwebsemantica/controllers/MainController.java
+++ src/main/java/br/usp/haroldo/epwebsemantica/controllers/MainController.java
@@ -53,26 +53,206 @@ public class MainController {
 }
 
 /*
+!!!!FILTER (?nome = "bakery") -> para buscar loja com certo nome   !!!!
+
 Listar Loja por nome
+@GetMapping("/lojas")
+    public List<LojaDTO> getLojas() throws IOException {
+        String queryString =
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                        "PREFIX parte_1: <http://www.web-semantica/ep/parte_1#>" +
+                        "SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                        "WHERE" +
+                          "{   { SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE
+                                  "{ ?uri  rdf:type    parte_1:LojaServicos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?uri      parte_1:oferece  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                            "UNION" +
+                              "{ SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE" +
+                                  "{ ?uri  rdf:type    parte_1:LojaProdutos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?uri      parte_1:vende  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                          "}";
 
-String queryString = 
-            "PREFIX owl: <http://www.w3.org/2002/07/owl#>"+
-            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
-            "SELECT ?label "+
-            "WHERE{ "+
-            "?x rdfs:label ?label "+
-            "FILTER (?x IN (rdf:type <http://www.web-semantica/ep/parte_1#LojaServicos/>,  rdf:type <http://www.web-semantica/ep/parte_1#LojaProdutos>))"; 
-Query query = QueryFactory.create(queryString);
-QueryExecution qe = QueryExecutionFactory.create(query, model);
-try {
-    ResultSet res = qe.execSelect();
-    while( res.hasNext()) {
-        QuerySolution soln = res.next();
-        RDFNode label = soln.get("?label");
-        System.out.println(label);
+        List<LojaDTO> lojas = ontology.executeQueryToType(queryString, LojaDTO.class);
+        return lojas;
     }
-} finally {
-    qe.close();
-}
+Listar Loja por atividade
+@GetMapping("/lojas")
+    public List<LojaDTO> getLojasByAtividade() throws IOException {
+        String queryString =
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                        "PREFIX parte_1: <http://www.web-semantica/ep/parte_1#>" +
+                        "SELECT  ?atividade ?uri ?nome ?atividadeRes ?fotoUrl" +
+                        "WHERE" +
+                          "{   { SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE
+                                  "{ ?uri  rdf:type    parte_1:LojaServicos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?uri      parte_1:oferece  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                            "UNION" +
+                              "{ SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE" +
+                                  "{ ?uri  rdf:type    parte_1:LojaProdutos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?uri      parte_1:vende  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                          "}" +
+                          "ORDER BY ?atividade";
 
+        List<LojaDTO> lojas = ontology.executeQueryToType(queryString, LojaDTO.class);
+        return lojas;
+    }
+Listar Loja por Orderm Alfabetica
+@GetMapping("/lojas")
+    public List<LojaDTO> getLojasOrdered() throws IOException {
+        String queryString =
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                        "PREFIX parte_1: <http://www.web-semantica/ep/parte_1#>" +
+                        "SELECT  ?nome ?uri ?atividadeRes ?atividade ?fotoUrl" +
+                        "WHERE" +
+                          "{   { SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE
+                                  "{ ?uri  rdf:type    parte_1:LojaServicos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?uri      parte_1:oferece  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                            "UNION" +
+                              "{ SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE" +
+                                  "{ ?uri  rdf:type    parte_1:LojaProdutos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?uri      parte_1:vende  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                          "}" +
+                          "ORDER BY ?nome";
+
+        List<LojaDTO> lojas = ontology.executeQueryToType(queryString, LojaDTO.class);
+        return lojas;
+    }
+Listar Rotas de Lojas 
+@GetMapping("/lojas")
+    public List<LojaDTO> getRotas() throws IOException {
+        String queryString =
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                        "PREFIX parte_1: <http://www.web-semantica/ep/parte_1#>" +
+                        "SELECT  ?nome ?rota ?uri ?atividadeRes ?atividade ?fotoUrl" +
+                        "WHERE" +
+                          "{   { SELECT  ?rota ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE
+                                  "{ ?uri  rdf:type    parte_1:LojaServicos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?rota     rdf:type         parte_1:Rota ;" +
+                                    "parte_1:levaA    ?uri ;" +
+                                    "rdfs:label       ?nomeRota ." +
+                                    "?uri      parte_1:oferece  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                            "UNION" +
+                              "{ SELECT  ?uri ?rota ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                                "WHERE" +
+                                  "{ ?uri  rdf:type    parte_1:LojaProdutos ;" +
+                                          "rdfs:label  ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                    "?rota     rdf:type         parte_1:Rota ;" +
+                                    "parte_1:levaA    ?uri ;" +
+                                    "rdfs:label       ?nomeRota ." +
+                                    "?uri      parte_1:vende  ?atividadeRes ." +
+                                    "?atividadeRes" +
+                                              "rdfs:label       ?atividade" +
+                                  "}" +
+                              "}" +
+                          "}" +
+                          "ORDER BY ?nome";
+
+        List<LojaDTO> lojas = ontology.executeQueryToType(queryString, LojaDTO.class);
+        return lojas;
+    }
+    
+Listar Produtos
+@GetMapping("/lojas")
+    public List<ProdutoDTO> getProducts() throws IOException {
+        String queryString =
+                "PREFIX owl: <http://www.w3.org/2002/07/owl#>" +
+                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                        "PREFIX parte_1: <http://www.web-semantica/ep/parte_1#>" +
+                        "SELECT  ?uri ?nome ?atividadeRes ?atividade ?fotoUrl" +
+                        "WHERE" +
+                          "{   { SELECT  ?nome ?uri ?fotoUrl" +
+                                "WHERE
+                                  "{ ?uriLoja  rdf:type    parte_1:LojaServicos ;" +
+                                          "rdfs:label  ?nomeLoja" +
+                                    "?uriLoja      parte_1:oferece  ?atividadeRes ." +
+                                    "?uri" +
+                                              "rdfs:label       ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                  "}" +
+                              "}" +
+                            "UNION" +
+                              "{ SELECT  ?nome ?uri ?fotoUrl" +
+                                "WHERE" +
+                                  "{ ?uriLoja  rdf:type    parte_1:LojaProdutos ;" +
+                                          "rdfs:label  ?nomeLoja" +
+                                    "?uriLoja      parte_1:vende  ?atividadeRes ." +
+                                    "?uri" +
+                                              "rdfs:label       ?nome" +
+                                    "OPTIONAL" +
+                                      "{ ?uri  parte_1:foto  ?fotoUrl }" +
+                                  "}" +
+                              "}" +
+                          "}";
+
+        List<ProdutoDTO> produtos = ontology.executeQueryToType(queryString, ProdutoDTO.class);
+        return produtos;
+    }
 */
